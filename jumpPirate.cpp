9f2f00ad83ed33d147a5cf57b738e0caf4f1ff33
Jordan Girardi <jordangirari000@gmail.com>
2021-03-22 19:32:47 -0300

    Atualização tamanho janela exibição

--- jumpPirate.cpp
+++ jumpPirate.cpp
@@ -37,7 +37,7 @@ int main( void )
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Open a window and create its OpenGL context
-	window = glfwCreateWindow( 1350, 750, "JumpPirate Game", NULL, NULL);
+	window = glfwCreateWindow( 1350, 768, "JumpPirate Game", NULL, NULL);
 	if( window == NULL ){
 		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
 		getchar();
